f922c94d78afeb5608329537888846699ee50c6f
Maxim Kuznetsov <mvk0402@gmail.com>
2025-04-22 19:45:16 +0300

    Update main.py

--- converter-api/app/main.py
+++ converter-api/app/main.py
@@ -13,7 +13,10 @@ app.include_router(routes.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://ipynb-converter.ru",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
